4f4404ada254bbbbcabeb744b6ebb5afe581ad41
csuvikv <csuvikv@inf.u-szeged.hu>
2019-04-25 14:47:27 +0200

    added email details

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ module.exports = {
     isAdmin:function(req) {
         return req.isAuthenticated() && req.session.passport.user && req.session.passport.user.admin == true ? true : false;
     }, 
-    sendMail:function(user, hotel, romm_number) {
+    sendMail:function(user, hotel, room_number) {
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -17,7 +17,15 @@ module.exports = {
             from: 'prfhotelapp@gmail.com',
             to: user.email,
             subject: 'Your reservation was successful!',
-            text: 'Dear ' + user.fullname + '!\n The details of your reservation: ' 
+            text: 'Dear ' + user.fullname + '!' +
+                  '\n Your reservetion in ' + hotel.fullname + ' was successful.' +
+                  '\n The details of your reservation:' +
+                  '\n Hotel: ' + hotel.fullname + 
+                  '\n Rooom number:' + room_number + 
+                  '\n Given name: ' + user.fullname +
+                  '\n Given email address: ' + user.email +
+                  '\n Best regards,' +
+                  '\n prf-hotel-app'
           };
           
         return transporter;

b0f2800fc20421b6224b90d3f0f74a23cc550d46
meszarosjeno <csuvikv@inf.u-szeged.hu>
2019-05-18 12:02:37 +0200

    cors

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const PORT = process.env.PORT || 5000
 const userModel = mongoose.model('user');
 
 const allowedOrigins = [
-    'https://prf-angular.herokuapp.com',
     'https://prf-angular.herokuapp.com/*'
 ];
 
